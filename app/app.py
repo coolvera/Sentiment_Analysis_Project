5d94e14a3060522d7ce031e2a2fc9dad3f9a71a2
coolvera <v.fombon0101@student.nu.edu>
2025-05-23 21:01:33 +0500

    Fix: bind Flask app to Heroku $PORT

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import joblib
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -21,4 +22,5 @@ def predict():
         return render_template('index.html', review=review, prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
